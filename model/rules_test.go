a015c03d3b2306a3baead2448404e5bb53c59bc6
Dave Shanley <dshanley@vmware.com>
2021-10-10 15:05:53 -0400

    working through resolving designs

--- model/rules_test.go
+++ model/rules_test.go
@@ -1,7 +1,7 @@
 package model
 
 import (
-	"github.com/quobix/vaccum/parser"
+	"github.com/daveshanley/vaccum/parser"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"testing"
@@ -9,11 +9,13 @@ import (
 
 func TestRuleSchema(t *testing.T) {
 
-	schema, err := ioutil.ReadFile("schemas/ruleset.schema.json")
-	assert.NoError(t, err)
+	//schema, err := ioutil.ReadFile("schemas/ruleset.schema.json")
+	//assert.NoError(t, err)
+	//
+	//goodRules, err := ioutil.ReadFile("../test_files/rules.json")
+	//assert.NoError(t, err)
 
-	goodRules, err := ioutil.ReadFile("../test_files/rules.json")
-	assert.NoError(t, err)
+	schemaFile = "file://"
 
 	r, err := parser.ValidateJSONAgainstSchema(schema, goodRules)
 	assert.NoError(t, err)
--- parser/schema.go
+++ parser/schema.go
@@ -2,14 +2,24 @@ package parser
 
 import "github.com/xeipuuv/gojsonschema"
 
-// CreateJSONSchemaLoaders will take in two byte arrays of JSON and build loaders read for validation.
 func CreateJSONSchemaLoaders(schema []byte, jsonToTest []byte) (sch gojsonschema.JSONLoader, doc gojsonschema.JSONLoader) {
 	sch = gojsonschema.NewStringLoader(string(schema))
 	doc = gojsonschema.NewStringLoader(string(jsonToTest))
 	return
 }
 
+func CreateJSONSchemaLoadersFromFiles(schema, jsonToTest string) (sch gojsonschema.JSONLoader, doc gojsonschema.JSONLoader) {
+	sch = gojsonschema.NewReferenceLoader(schema)
+	doc = gojsonschema.NewReferenceLoader(jsonToTest)
+	return
+}
+
 func ValidateJSONAgainstSchema(schema []byte, jsonToTest []byte) (*gojsonschema.Result, error) {
 	schemaLoader, inputLoader := CreateJSONSchemaLoaders(schema, jsonToTest)
+	return gojsonschema.Validate(schemaLoader, inputLoader)
+}
+
+func ValidateJSONAgainstSchemaUsingFiles(schema, jsonToTest string) (*gojsonschema.Result, error) {
+	schemaLoader, inputLoader := CreateJSONSchemaLoadersFromFiles(schema, jsonToTest)
 	return gojsonschema.Validate(schemaLoader, inputLoader)
 }
