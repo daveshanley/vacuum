058f61dfaa2710ec8e6e031f580fa38419934a49
Dave Shanley <dshanley@vmware.com>
2021-12-23 12:50:15 -0500

    bumping coverage on pattern tests

--- functions/core/pattern_test.go
+++ functions/core/pattern_test.go
@@ -25,15 +25,31 @@ func TestCasing_RunRule_PatternMatchSuccess(t *testing.T) {
 	assert.Len(t, res, 0)
 }
 
-func TestCasing_RunRule_PatternMatchError(t *testing.T) {
+func TestCasing_RunRule_PatternNothingSupplied(t *testing.T) {
 
 	sampleYaml := `carpet: "abc"`
 	path := "$.carpet"
 	nodes, _ := utils.FindNodes([]byte(sampleYaml), path)
 	assert.Len(t, nodes, 1)
 
+	rule := buildCoreTestRule(path, severityError, "pattern", "", nil)
+	ctx := buildCoreTestContextFromRule(rule.Then, rule)
+
+	def := &Pattern{}
+	res := def.RunRule(nodes, ctx)
+
+	assert.Len(t, res, 0)
+}
+
+func TestCasing_RunRule_PatternNotMatchError(t *testing.T) {
+
+	sampleYaml := `carpet: "nice-rice"`
+	path := "$.carpet"
+	nodes, _ := utils.FindNodes([]byte(sampleYaml), path)
+	assert.Len(t, nodes, 1)
+
 	opts := make(map[string]string)
-	opts["match"] = "([abc]"
+	opts["notMatch"] = "[[abc)"
 
 	rule := buildCoreTestRule(path, severityError, "pattern", "", opts)
 	ctx := buildCoreTestContextFromRule(rule.Then, rule)
@@ -53,6 +69,25 @@ func TestCasing_RunRule_PatternMatchFail(t *testing.T) {
 
 	opts := make(map[string]string)
 	opts["match"] = "[abc]+"
+
+	rule := buildCoreTestRule(path, severityError, "pattern", "", opts)
+	ctx := buildCoreTestContextFromRule(rule.Then, rule)
+
+	def := &Pattern{}
+	res := def.RunRule(nodes, ctx)
+
+	assert.Len(t, res, 1)
+}
+
+func TestCasing_RunRule_PatternMatchError(t *testing.T) {
+
+	sampleYaml := `carpet: "abc"`
+	path := "$.carpet"
+	nodes, _ := utils.FindNodes([]byte(sampleYaml), path)
+	assert.Len(t, nodes, 1)
+
+	opts := make(map[string]string)
+	opts["match"] = "([abc]"
 
 	rule := buildCoreTestRule(path, severityError, "pattern", "", opts)
 	ctx := buildCoreTestContextFromRule(rule.Then, rule)
