041fcb574cfda5229ef48c60fd6239ff2d48d368
Dave Shanley <dave@quobix.com>
2021-12-29 12:12:15 -0500

    bumped coverage on utils tests

--- functions/openapi/openapi_utils_test.go
+++ functions/openapi/openapi_utils_test.go
@@ -12,8 +12,25 @@ func TestGetTagsFromRoot(t *testing.T) {
 	nodes, _ := utils.FindNodes(sampleYaml, "$")
 	assert.Len(t, GetTagsFromRoot(nodes), 2)
 }
+
+func TestGetTagsFromRoot_Fail(t *testing.T) {
+	sampleYaml, _ := ioutil.ReadFile("../../model/test_files/burgershop.openapi.yaml")
+	nodes, _ := utils.FindNodes(sampleYaml, "$.does-not-exist")
+	assert.Len(t, GetTagsFromRoot(nodes), 0)
+}
+
 func TestGetOperationsFromRoot(t *testing.T) {
 	sampleYaml, _ := ioutil.ReadFile("../../model/test_files/burgershop.openapi.yaml")
 	nodes, _ := utils.FindNodes(sampleYaml, "$")
 	assert.Len(t, GetOperationsFromRoot(nodes), 10) // this is 5 paths and sequential map nodes.
+}
+
+func TestGetOperationsFromRoot_Fail(t *testing.T) {
+	sampleYaml, _ := ioutil.ReadFile("../../model/test_files/burgershop.openapi.yaml")
+	nodes, _ := utils.FindNodes(sampleYaml, "$.made-up-nothing")
+	assert.Len(t, GetOperationsFromRoot(nodes), 0)
+}
+
+func TestGetAllOperationsJSONPath(t *testing.T) {
+	assert.NotNil(t, GetAllOperationsJSONPath())
 }
