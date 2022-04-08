8c54bec09c53b860445d67debfa9ae437d159459
Dave Shanley <dave@quobix.com>
2022-04-08 06:28:04 -0400

    Bumping coverage on pattern function
    Signed-off-by: Dave Shanley <dave@quobix.com>

--- functions/core/pattern_test.go
+++ functions/core/pattern_test.go
@@ -7,6 +7,17 @@ import (
 	"testing"
 )
 
+func TestPattern_GetSchema(t *testing.T) {
+	def := Pattern{}
+	assert.Equal(t, "pattern", def.GetSchema().Name)
+}
+
+func TestPattern_RunRule(t *testing.T) {
+	def := Pattern{}
+	res := def.RunRule(nil, model.RuleFunctionContext{})
+	assert.Len(t, res, 0)
+}
+
 func TestPattern_RunRule_PatternMatchSuccess(t *testing.T) {
 
 	sampleYaml := `carpet: "abc"`
@@ -128,4 +139,48 @@ func TestPattern_RunRule_PatternNotMatchFail(t *testing.T) {
 	res := def.RunRule(nodes, ctx)
 
 	assert.Len(t, res, 1)
+}
+
+func TestPattern_RunRule_UseFieldName(t *testing.T) {
+
+	sampleYaml := `no: 
+  sleep: until`
+	path := "$.no"
+	nodes, _ := utils.FindNodes([]byte(sampleYaml), path)
+	assert.Len(t, nodes, 1)
+
+	opts := make(map[string]string)
+	opts["match"] = "cake"
+
+	rule := buildCoreTestRule(path, severityError, "pattern", "sleep", opts)
+	ctx := buildCoreTestContextFromRule(model.CastToRuleAction(rule.Then), rule)
+	ctx.Given = path
+	ctx.Rule = &rule
+
+	def := &Pattern{}
+	res := def.RunRule(nodes, ctx)
+
+	assert.Len(t, res, 1)
+}
+
+func TestPattern_RunRule_ContainMap(t *testing.T) {
+
+	sampleYaml := `no: 
+  sleep: until`
+	path := "$"
+	nodes, _ := utils.FindNodes([]byte(sampleYaml), path)
+	assert.Len(t, nodes, 1)
+
+	opts := make(map[string]string)
+	opts["match"] = "until"
+
+	rule := buildCoreTestRule(path, severityError, "pattern", "sleep", opts)
+	ctx := buildCoreTestContextFromRule(model.CastToRuleAction(rule.Then), rule)
+	ctx.Given = path
+	ctx.Rule = &rule
+
+	def := &Pattern{}
+	res := def.RunRule(nodes, ctx)
+
+	assert.Len(t, res, 0)
 }
