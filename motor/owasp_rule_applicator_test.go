091791966de73affadedb29fa0be216ba520fa08
ricagraca <ricardo.graca@freiheit.com>
2023-06-05 23:18:43 +0100

    add new rule

--- motor/owasp_rule_applicator_test.go
+++ motor/owasp_rule_applicator_test.go
@@ -824,3 +824,30 @@ paths:
 	results := ApplyRulesToRuleSet(rse)
 	assert.Len(t, results.Results, 1)
 }
+
+func TestRuleSet_GetOWASPRuleNoAdditionalPropertiesSuccess(t *testing.T) {
+
+	yml := `openapi: "3.0.0"
+info:
+  version: "1.0"
+components:
+  schemas:
+    Foo:
+      type: object
+      additionalProperties: false
+`
+
+	rules := make(map[string]*model.Rule)
+	rules["here"] = rulesets.GetOWASPRuleNoAdditionalProperties() // TODO
+
+	rs := &rulesets.RuleSet{
+		Rules: rules,
+	}
+
+	rse := &RuleSetExecution{
+		RuleSet: rs,
+		Spec:    []byte(yml),
+	}
+	results := ApplyRulesToRuleSet(rse)
+	assert.Len(t, results.Results, 0)
+}
