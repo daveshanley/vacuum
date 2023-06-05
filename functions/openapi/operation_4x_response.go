ea225c2565d602275369daf9771fce6a26b31f7c
ricagraca <ricardo.graca@freiheit.com>
2023-06-05 19:20:16 +0100

    done

--- functions/openapi/operation_4x_response.go
+++ functions/openapi/operation_4x_response.go
@@ -4,12 +4,11 @@
 package openapi
 
 import (
-	"fmt"
-	"strconv"
-
-	"github.com/daveshanley/vacuum/model"
-	"github.com/pb33f/libopenapi/utils"
-	"gopkg.in/yaml.v3"
+    "fmt"
+    "github.com/daveshanley/vacuum/model"
+    "github.com/pb33f/libopenapi/utils"
+    "gopkg.in/yaml.v3"
+    "strconv"
 )
 
 // Operation4xResponse is a rule that checks if an operation returns a 4xx (user error) code.
@@ -18,70 +17,59 @@ type Operation4xResponse struct {
 
 // GetSchema returns a model.RuleFunctionSchema defining the schema of the SuccessResponse rule.
 func (or Operation4xResponse) GetSchema() model.RuleFunctionSchema {
-	return model.RuleFunctionSchema{Name: "operation_4xx_response"}
+    return model.RuleFunctionSchema{Name: "operation_4xx_response"}
 }
 
 // RunRule will execute the Operation4xResponse rule, based on supplied context and a supplied []*yaml.Node slice.
 func (or Operation4xResponse) RunRule(nodes []*yaml.Node, context model.RuleFunctionContext) []model.RuleFunctionResult {
 
-	if len(nodes) <= 0 {
-		return nil
-	}
+    if len(nodes) <= 0 {
+        return nil
+    }
 
-	var results []model.RuleFunctionResult
+    var results []model.RuleFunctionResult
 
-	if context.Index.GetPathsNode() == nil {
-		return results
-	}
-	ops := context.Index.GetPathsNode().Content
+    if context.Index.GetPathsNode() == nil {
+        return results
+    }
+    ops := context.Index.GetPathsNode().Content
 
-	// check if should validate default error code (e.g., 4xx)
-	s := utils.ExtractValueFromInterfaceMap("owaspEnabled", context.Options)
-	owaspEnabled := s.(bool)
+    var opPath, opMethod string
+    for i, op := range ops {
+        if i%2 == 0 {
+            opPath = op.Value
+            continue
+        }
+        for m, method := range op.Content {
+            if m%2 == 0 {
+                opMethod = method.Value
+                continue
+            }
+            basePath := fmt.Sprintf("$.paths.%s.%s", opPath, opMethod)
+            _, responsesNode := utils.FindKeyNode("responses", method.Content)
 
-	var opPath, opMethod string
-	for i, op := range ops {
-		if i%2 == 0 {
-			opPath = op.Value
-			continue
-		}
-		for m, method := range op.Content {
-			if m%2 == 0 {
-				opMethod = method.Value
-				continue
-			}
-			basePath := fmt.Sprintf("$.paths.%s.%s", opPath, opMethod)
-			_, responsesNode := utils.FindKeyNode("responses", method.Content)
-
-			if responsesNode != nil {
-				seen := false
-				for k, response := range responsesNode.Content {
-					if k%2 != 0 {
-						continue
-					}
-					if owaspEnabled {
-						responseCode, _ := strconv.Atoi(response.Value)
-						if response.Value == "4XX" || responseCode == 400 || responseCode == 422 {
-							seen = true
-						}
-					} else {
-						responseCode, _ := strconv.Atoi(response.Value)
-						if responseCode >= 400 && responseCode <= 499 {
-							seen = true
-						}
-					}
-				}
-				if !seen {
-					results = append(results, model.RuleFunctionResult{
-						Message:   "Operation must define at least one 4xx error response",
-						StartNode: method,
-						EndNode:   utils.FindLastChildNodeWithLevel(method, 0),
-						Path:      basePath,
-						Rule:      context.Rule,
-					})
-				}
-			}
-		}
-	}
-	return results
+            if responsesNode != nil {
+                seen := false
+                for k, response := range responsesNode.Content {
+                    if k%2 != 0 {
+                        continue
+                    }
+                    responseCode, _ := strconv.Atoi(response.Value)
+                    if responseCode >= 400 && responseCode <= 499 {
+                        seen = true
+                    }
+                }
+                if !seen {
+                    results = append(results, model.RuleFunctionResult{
+                        Message:   "Operation must define at least one 4xx error response",
+                        StartNode: method,
+                        EndNode:   utils.FindLastChildNodeWithLevel(method, 0),
+                        Path:      basePath,
+                        Rule:      context.Rule,
+                    })
+                }
+            }
+        }
+    }
+    return results
 }
