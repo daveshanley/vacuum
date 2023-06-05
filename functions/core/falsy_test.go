85957946062087075243c975d03cd0a9b3b28bde
ricagraca <ricardo.graca@freiheit.com>
2023-06-05 23:34:49 +0100

    Fixes falsy

--- functions/core/falsy_test.go
+++ functions/core/falsy_test.go
@@ -1,28 +1,28 @@
 package core
 
 import (
+	"testing"
+
 	"github.com/daveshanley/vacuum/model"
 	"github.com/pb33f/libopenapi/utils"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestFalsy_RunRule_Fail(t *testing.T) {
 
 	sampleYaml := `
 tags:
-  - name: "bad tag 1"
-    description: false
-  - name: "bad tag 2"
-    description: 0
-  - name: "bad tag 3"
-    description: ""
-  - name: "bad tag 4"`
+  - name: "good tag 1"
+    description: true
+  - name: "good tag 2"
+    description: 1
+  - name: "good tag 3"
+    description: "hello"`
 
 	path := "$.tags[*]"
 
 	nodes, _ := utils.FindNodes([]byte(sampleYaml), path)
-	assert.Len(t, nodes, 4)
+	assert.Len(t, nodes, 3)
 
 	rule := buildCoreTestRule(path, model.SeverityError, "falsy", "description", nil)
 	ctx := buildCoreTestContext(model.CastToRuleAction(rule.Then), nil)
@@ -44,7 +44,7 @@ notTags:
  - name: "bad tag 2"
    description: 0
  - name: "bad tag 3"
-   description: ""
+   description: "32"
  - name: "bad tag 4"`
 
 	path := "$.tags[*]"
@@ -68,11 +68,12 @@ func TestFalsy_RunRule_Pass(t *testing.T) {
 	sampleYaml := `
 tags:
  - name: "good tag 1"
- - name: "bad tag 2"
- - name: "bad tag 3"
+ - name: "good tag 2"
+   description: "false"
+ - name: "good tag 3"
    description: ""
- - name: "good Tag 2"
-   description: "a nice description"`
+ - name: "good Tag 4"
+   description: "0"`
 
 	path := "$.tags[*]"
 
@@ -87,5 +88,5 @@ tags:
 	tru := Falsy{}
 	res := tru.RunRule(nodes, ctx)
 
-	assert.Len(t, res, 2)
+	assert.Len(t, res, 0)
 }
