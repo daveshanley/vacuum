6b380564c6e38df9074dab15ea2eec7ef8ab00c2
ricagraca <ricardo.graca@freiheit.com>
2023-06-05 23:56:47 +0100

    fix

--- functions/core/falsy.go
+++ functions/core/falsy.go
@@ -4,11 +4,11 @@
 package core
 
 import (
-  "fmt"
+    "fmt"
 
-  "github.com/daveshanley/vacuum/model"
-  "github.com/pb33f/libopenapi/utils"
-  "gopkg.in/yaml.v3"
+    "github.com/daveshanley/vacuum/model"
+    "github.com/pb33f/libopenapi/utils"
+    "gopkg.in/yaml.v3"
 )
 
 // Falsy is a rule that will determine if something is seen as 'false' (could be a 0 or missing, or actually 'false')
@@ -17,39 +17,39 @@ type Falsy struct {
 
 // GetSchema returns a model.RuleFunctionSchema defining the schema of the Falsy rule.
 func (f Falsy) GetSchema() model.RuleFunctionSchema {
-  return model.RuleFunctionSchema{
-    Name: "falsy",
-  }
+    return model.RuleFunctionSchema{
+        Name: "falsy",
+    }
 }
 
 // RunRule will execute the Falsy rule, based on supplied context and a supplied []*yaml.Node slice.
 func (f Falsy) RunRule(nodes []*yaml.Node, context model.RuleFunctionContext) []model.RuleFunctionResult {
 
-  if len(nodes) <= 0 {
-    return nil
-  }
-
-  var results []model.RuleFunctionResult
-
-  pathValue := "unknown"
-  if path, ok := context.Given.(string); ok {
-    pathValue = path
-  }
-
-  for _, node := range nodes {
-
-    fieldNode, fieldNodeValue := utils.FindKeyNode(context.RuleAction.Field, node.Content)
-    if (fieldNode != nil && fieldNodeValue != nil) &&
-      (fieldNodeValue.Value != "" && fieldNodeValue.Value != "false" || fieldNodeValue.Value != "0") {
-      results = append(results, model.RuleFunctionResult{
-        Message:   fmt.Sprintf("%s: '%s' must be falsy", context.Rule.Description, context.RuleAction.Field),
-        StartNode: node,
-        EndNode:   node,
-        Path:      pathValue,
-        Rule:      context.Rule,
-      })
+    if len(nodes) <= 0 {
+        return nil
     }
-  }
 
-  return results
+    var results []model.RuleFunctionResult
+
+    pathValue := "unknown"
+    if path, ok := context.Given.(string); ok {
+        pathValue = path
+    }
+
+    for _, node := range nodes {
+
+        fieldNode, fieldNodeValue := utils.FindKeyNode(context.RuleAction.Field, node.Content)
+        if (fieldNode != nil && fieldNodeValue != nil) &&
+            (fieldNodeValue.Value != "" && fieldNodeValue.Value != "false" || fieldNodeValue.Value != "0") {
+            results = append(results, model.RuleFunctionResult{
+                Message:   fmt.Sprintf("%s: '%s' must be falsy", context.Rule.Description, context.RuleAction.Field),
+                StartNode: node,
+                EndNode:   node,
+                Path:      pathValue,
+                Rule:      context.Rule,
+            })
+        }
+    }
+
+    return results
 }
