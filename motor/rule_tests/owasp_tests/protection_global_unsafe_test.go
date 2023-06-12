f098f00e2fadeec91bc123df1e1277c6cbaf28f5
Ricagraca <ricardo.graca@freiheit.com>
2023-06-12 19:31:52 +0100

    Fixes tests

--- motor/rule_tests/owasp_tests/protection_global_unsafe_test.go
+++ motor/rule_tests/owasp_tests/protection_global_unsafe_test.go
@@ -16,16 +16,16 @@ info:
   version: "1.2.3"
   title: "securitySchemes"
 paths:
-  /security-ko-missing:
-    put:
-      responses: {}
-    post:
-      security: []
   /security-ok-put:
     put:
       security:
         - BasicAuth: []
       responses: {}
+  /security-ok-get:
+    get:
+      responses: {}
+    head:
+      security: []
   /security-ok-get:
     get:
       security:
@@ -35,10 +35,40 @@ paths:
       security:
         - {}
         - BasicAuth: []
-  /security-ko-get:
-    get:
+components:
+  securitySchemes:
+    BasicAuth:
+      type: http
+      scheme: basic`
+
+	t.Run("valid case", func(t *testing.T) {
+		rules := make(map[string]*model.Rule)
+		rules["here"] = rulesets.GetOWASPRuleProtectionGlobalUnsafe()
+
+		rs := &rulesets.RuleSet{
+			Rules: rules,
+		}
+
+		rse := &motor.RuleSetExecution{
+			RuleSet: rs,
+			Spec:    []byte(yml),
+		}
+		results := motor.ApplyRulesToRuleSet(rse)
+		assert.Len(t, results.Results, 0)
+	})
+}
+
+func TestRuleSet_TestGetOWASPRuleProtectionGlobalUnsafe_Error(t *testing.T) {
+
+	yml := `openapi: 3.0.1
+info:
+  version: "1.2.3"
+  title: "securitySchemes"
+paths:
+  /security-ko-missing:
+    put:
       responses: {}
-    head:
+    post:
       security: []
   /security-ko-info:
     post:
@@ -65,54 +95,5 @@ components:
 		}
 		results := motor.ApplyRulesToRuleSet(rse)
 		assert.Len(t, results.Results, 2)
-	})
-}
-
-func TestRuleSet_TestGetOWASPRuleProtectionGlobalUnsafe_Error(t *testing.T) {
-
-	// TODO here
-
-	yml := `openapi: "3.1.0"
-paths:
-  /foo/{id}/:
-    get:
-      description: "get"
-      parameters:
-        - name: id
-          in: path
-          schema:
-            type: integer
-        - name: notanid
-          in: path
-          schema:
-            type: integer
-        - name: underscore_id
-          in: path
-          schema:
-            type: integer
-        - name: hyphen-id
-          in: path
-          schema:
-            type: integer
-            format: int32
-        - name: camelId
-          in: path
-          schema:
-            type: integer`
-
-	t.Run("invalid case", func(t *testing.T) {
-		rules := make(map[string]*model.Rule)
-		rules["here"] = rulesets.GetOWASPRuleProtectionGlobalUnsafe()
-
-		rs := &rulesets.RuleSet{
-			Rules: rules,
-		}
-
-		rse := &motor.RuleSetExecution{
-			RuleSet: rs,
-			Spec:    []byte(yml),
-		}
-		results := motor.ApplyRulesToRuleSet(rse)
-		assert.Len(t, results.Results, 5)
 	})
 }
