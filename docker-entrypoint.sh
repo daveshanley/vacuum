aa1d31cb5fed4363b787ed271e8fca7162c8ddb8
xwillq <xwillq@pm.me>
2023-08-11 13:43:49 +0500

    Added docker entrypoint script

---
+++ docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+# If supplied command starts with `-`, we assume it is a flag for vacuum,
+# otherwise check if command is a valid vacuum subcommand.
+# If it is, prepend `vacuum` to script parameters:
+# if `$@` is `lint`, it becomes `vacuum lint`
+if [ "${1#-}" != "$1" ] || vacuum "$1" --help > /dev/null 2>&1; then
+    set -- vacuum "$@"
+fi
+
+exec "$@"
