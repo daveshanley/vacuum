43f20aeb64e2a0b7758a279d87a4c5be269acb83
quobix <dave@quobix.com>
2023-09-16 10:50:38 -0400

    Cleaned up some static check errors.
    Signed-off-by: quobix <dave@quobix.com>

--- cui/snippet.go
+++ cui/snippet.go
@@ -3,39 +3,39 @@ package cui
 import (
 	"image"
 
-	. "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3"
 )
 
 type Snippet struct {
-	Block
+	termui.Block
 	Text      string
-	TextStyle Style
+	TextStyle termui.Style
 	WrapText  bool
 }
 
 func NewSnippet() *Snippet {
 	return &Snippet{
-		Block:     *NewBlock(),
-		TextStyle: Theme.Paragraph.Text,
+		Block:     *termui.NewBlock(),
+		TextStyle: termui.Theme.Paragraph.Text,
 		WrapText:  false,
 	}
 }
 
-func (snip *Snippet) Draw(buf *Buffer) {
+func (snip *Snippet) Draw(buf *termui.Buffer) {
 	snip.Block.Draw(buf)
 
-	cells := ParseStyles(snip.Text, snip.TextStyle)
+	cells := termui.ParseStyles(snip.Text, snip.TextStyle)
 	if snip.WrapText {
-		cells = WrapCells(cells, uint(snip.Inner.Dx()))
+		cells = termui.WrapCells(cells, uint(snip.Inner.Dx()))
 	}
 
-	rows := SplitCells(cells, '\n')
+	rows := termui.SplitCells(cells, '\n')
 	for y, row := range rows {
 		if y+snip.Inner.Min.Y >= snip.Inner.Max.Y {
 			break
 		}
-		row = TrimCells(row, snip.Inner.Dx())
-		for _, cx := range BuildCellWithXArray(row) {
+		row = termui.TrimCells(row, snip.Inner.Dx())
+		for _, cx := range termui.BuildCellWithXArray(row) {
 			x, cell := cx.X, cx.Cell
 			buf.SetCell(cell, image.Pt(x, y).Add(snip.Inner.Min))
 		}
