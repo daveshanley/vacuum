9b73e7c9a9277a8a1c8b76a7a1836aa46a0301b1
quobix <dave@quobix.com>
2024-04-12 11:00:36 -0400

    missing examples are now sensitive to encased parent objects
    for example, currently a schema will complain it has no example, even if the enclosing parent does. like a parameter having an example, and a schema that does not, the rule will be triggered.
    
    This fixes that with some updates to the doctor.
    
    bumped deps also.

--- functions/openapi/examples_missing_test.go
+++ functions/openapi/examples_missing_test.go
@@ -61,6 +61,45 @@ paths:
 	assert.Contains(t, res[0].Path, "$.paths['/pizza'].get.requestBody.content['application/json']")
 }
 
+func TestExamplesMissing_TrainTravel(t *testing.T) {
+
+	yml := `openapi: 3.1
+paths:
+  /trips:
+    get:
+      parameters:
+        - name: origin
+          in: query
+          schema:
+            type: string
+            format: uuid
+          example: efdbb9d1-02c2-4bc3-afb7-6788d8782b1ee`
+
+	document, err := libopenapi.NewDocument([]byte(yml))
+	if err != nil {
+		panic(fmt.Sprintf("cannot create new document: %e", err))
+	}
+
+	m, _ := document.BuildV3Model()
+	path := "$"
+
+	drDocument := drModel.NewDrDocument(m)
+
+	rule := buildOpenApiTestRuleAction(path, "examples_missing", "", nil)
+	ctx := buildOpenApiTestContext(model.CastToRuleAction(rule.Then), nil)
+
+	ctx.Document = document
+	ctx.DrDocument = drDocument
+	ctx.Rule = &rule
+
+	def := ExamplesMissing{}
+	res := def.RunRule(nil, ctx)
+
+	assert.Len(t, res, 0)
+	//assert.Equal(t, "schema is missing `examples` or `example`", res[0].Message)
+	//assert.Contains(t, res[0].Path, "$.paths['/pizza'].get.requestBody.content['application/json']")
+}
+
 func TestExamplesMissing_Alt(t *testing.T) {
 
 	yml := `openapi: 3.1
