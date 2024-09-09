622c9f393d64436d61317903111d34a3e904f869
quobix <dave@quobix.com>
2024-09-09 19:35:39 -0400

    Added support for the doctor to core rules
    Now the doctor can be used with all core rules!

--- functions/core/truthy.go
+++ functions/core/truthy.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"github.com/daveshanley/vacuum/model"
 	vacuumUtils "github.com/daveshanley/vacuum/utils"
+	"github.com/pb33f/doctor/model/high/base"
 	"github.com/pb33f/libopenapi/utils"
 	"gopkg.in/yaml.v3"
 	"sort"
@@ -89,14 +90,18 @@ func (t *Truthy) RunRule(nodes []*yaml.Node, context model.RuleFunctionContext) 
 				if err == nil && locatedObject != nil {
 					locatedPath = locatedObject.GenerateJSONPath()
 				}
-				results = append(results, model.RuleFunctionResult{
+				result := model.RuleFunctionResult{
 					Message: vacuumUtils.SuppliedOrDefault(message,
 						fmt.Sprintf("%s: `%s` must be set", ruleMessage, context.RuleAction.Field)),
 					StartNode: node,
 					EndNode:   vacuumUtils.BuildEndNode(node),
 					Path:      locatedPath,
 					Rule:      context.Rule,
-				})
+				}
+				results = append(results, result)
+				if arr, ok := locatedObject.(base.AcceptsRuleResults); ok {
+					arr.AddRuleFunctionResult(base.ConvertRuleResult(&result))
+				}
 			}
 		}
 	}
