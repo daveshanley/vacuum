7291462e9539297709ae111676b3d465f940bf7c
quobix <dave@quobix.com>
2025-08-04 20:53:48 -0400

    Address issue #681
    custom functions are now supported in reports.

--- cmd/spectral_report.go
+++ cmd/spectral_report.go
@@ -129,13 +129,11 @@ func GetSpectralReportCommand() *cobra.Command {
 			}
 
 			functionsFlag, _ := cmd.Flags().GetString("functions")
-			var customFunctions map[string]model.RuleFunction
+			customFunctions, _ := LoadCustomFunctions(functionsFlag, true)
 
 			// if ruleset has been supplied, lets make sure it exists, then load it in
 			// and see if it's valid. If so - let's go!
 			if rulesetFlag != "" {
-
-				customFunctions, _ = LoadCustomFunctions(functionsFlag, true)
 				var rsErr error
 				selectedRS, rsErr = BuildRuleSetFromUserSuppliedLocation(rulesetFlag, defaultRuleSets, remoteFlag)
 				if rsErr != nil {
