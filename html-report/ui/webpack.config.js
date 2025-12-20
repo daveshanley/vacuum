a47aa572730ba41f0bfa2a64315c3166b547fa70
quobix <dave@quobix.com>
2025-12-20 09:01:07 -0500

    updated deps and examples error reporting

--- html-report/ui/webpack.config.js
+++ html-report/ui/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
+        exclude: [/node_modules/, /\.test\.ts$/],
       },
       {
         test: /\.css$/,
