7e3fca041451d95ac39ab0019fa17f2229a2b20c
quobix <dave@quobix.com>
2025-12-24 15:16:17 -0500

    update more headers

--- plugin/javascript/eventloop/eventloop.go
+++ plugin/javascript/eventloop/eventloop.go
@@ -1,4 +1,4 @@
-// Copyright 2023-2025 Princess B33f Heavy Industries / Dave Shanley
+// Copyright 2023-2025 Princess Beef Heavy Industries / Dave Shanley
 // SPDX-License-Identifier: MIT
 
 // Package eventloop provides an event loop implementation for running asynchronous
--- vacuum-report/junit_test.go
+++ vacuum-report/junit_test.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Princess B33f Heavy Industries / Dave Shanley
+// Copyright 2023 Princess Beef Heavy Industries / Dave Shanley
 // SPDX-License-Identifier: MIT
 
 package vacuum_report
