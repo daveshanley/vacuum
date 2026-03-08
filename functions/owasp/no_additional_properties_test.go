295475afd8c406314c89843311a67da7f0daf16c
quobix <dave@quobix.com>
2026-03-08 09:10:23 -0400

    addressing additional checks to additional properties

--- functions/owasp/no_additional_properties_test.go
+++ functions/owasp/no_additional_properties_test.go
@@ -84,7 +84,8 @@ components:
 
 	res := def.RunRule(nil, ctx)
 
-	assert.Len(t, res, 0)
+	assert.Len(t, res, 1)
+	assert.Equal(t, "`additionalProperties` should not be set, or set to `false`", res[0].Message)
 }
 
 func TestNoAdditionalProperties_RunRule_Pass(t *testing.T) {
