afc41c2bbb6c45bc4814b63ae0b28ea70a30c06c
quobix <dave@quobix.com>
2026-03-29 18:01:04 -0400

    bumping all deps to cleanup security.

--- html-report/ui/build/static/js/hydrate.js
+++ html-report/ui/build/static/js/hydrate.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";setTimeout((function(){const t=document.querySelector("category-piechart");t&&t.setChartData(window.statistics)}),200)})();
+(()=>{"use strict";setTimeout(function(){const t=document.querySelector("category-piechart");t&&t.setChartData(window.statistics)},200)})();
